e4efffd15f7252c9d99bfd97832cc9dc4eb34942
Luto Yvan <zuoez02@outlook.com>
2024-01-24 06:05:34 +0000

    v0.3.3: fix in v2.12.4

--- src/components/dock.jsx
+++ src/components/dock.jsx
@@ -63,7 +63,7 @@ export const Dock = (props) => {
     <div className="fn__flex-1 fn__flex-column">
       <div className="block__icons">
         <div className="block__logo">
-          <svg>
+          <svg className="block__logoicon">
             <use xlinkHref="#iconExcalidraw"></use>
           </svg>
           Excalidraw
